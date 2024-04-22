a38fdc8dd2a5ac67566c5834c95de49d819835f0
Lukas Schmitt <lukas.schmitt@iteratec.com>
2024-04-22 16:50:01 +0200

    Initial commit

---
+++ src/App.svelte
@@ -0,0 +1,24 @@
+<script>
+  import { onMount } from 'svelte';
+  import Test from './Test.svelte';
+
+  let container;
+
+  onMount(() => {
+    if (!window.customElements.get('ce-test')) {
+        window.customElements.define('ce-test', Test.element);
+    }
+
+    const customElement = document.createElement('ce-test');
+
+    customElement.name = 'accessor-name';
+    customElement.other = 'accessor-other';
+
+    container.appendChild(customElement);
+  })
+
+</script>
+ 
+<ce-test name="attribute-name" other="attribute-other"></ce-test>
+
+<div bind:this={container} />
---
+++ src/Test.svelte
@@ -0,0 +1,11 @@
+<svelte:options
+    customElement={{
+        tag: null,
+    }}
+/>
+<script>
+	export let name;
+</script>
+
+<p>$$props: {JSON.stringify($$props)}</p>
+<p>$$restProps: {JSON.stringify($$restProps)}</p>
